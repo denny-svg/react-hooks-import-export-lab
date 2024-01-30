e38ed586a8226484f2ba5be4725425533e67c341
Denny-svg <code_sakata@sakataas>
2024-01-30 14:36:03 +0300

    Done assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import About from "./About";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import{username, city, image} from "/home/code_sakata/react-hooks-import-export-lab/src/data/user.js"
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city}/>
+      <About image={image}/>
     </div>
   );
 }
